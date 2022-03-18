29b8b4d22ddba61289c195f1ff6504e0f039ba39
Hayden Bleasel <hello@haydenbleasel.com>
2022-03-18 16:29:07 +1100

    Create Stylelint configuration

---
+++ index.js
@@ -0,0 +1,9 @@
+import eslint from "./src/eslint";
+import prettier from "./src/prettier";
+import stylelint from "./src/stylelint";
+
+module.exports = {
+  eslint,
+  prettier,
+  stylelint,
+};
---
+++ src/stylelint.js
@@ -0,0 +1,38 @@
+module.exports = {
+  plugins: ['stylelint-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-idiomatic-order',
+    'stylelint-config-prettier',
+  ],
+  rules: {
+    'prettier/prettier': true,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'layer',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'declaration-block-no-redundant-longhand-properties': [
+      true,
+      {
+        ignoreShorthands: ['/flex/'],
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
+};
