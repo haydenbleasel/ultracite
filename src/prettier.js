7752a996348cc9e82f610efb67fb704b8f41847b
Hayden Bleasel <hello@haydenbleasel.com>
2022-03-18 16:27:02 +1100

    Create Prettier configuration

---
+++ src/prettier.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "bracketSpacing": true,
+  "arrowParens": "always",
+  "proseWrap": "never",
+  "printWidth": 80
+};
