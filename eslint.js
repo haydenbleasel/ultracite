1c31b1585c593fafcaba1b37714669d974e04329
Hayden Bleasel <hello@haydenbleasel.com>
2022-11-18 19:39:18 -0500

    Allow finally in promises

--- eslint.js
+++ eslint.js
@@ -624,7 +624,7 @@ module.exports = {
     'promise/always-return': 'error',
     'promise/no-return-wrap': 'error',
     'promise/param-names': 'error',
-    'promise/catch-or-return': 'error',
+    'promise/catch-or-return': ['error', { allowFinally: true }],
     'promise/no-native': 'off',
     'promise/no-nesting': 'error',
     'promise/no-promise-in-callback': 'error',
