dc0c134cb1016460056a51e832307eddce2c1917
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-14 10:30:51 -0500

    Remove tsconfig from base config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,110 +1,12 @@
-/* eslint-disable import/no-extraneous-dependencies, n/no-extraneous-import, id-length */
-
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import * as typescript from '@typescript-eslint/eslint-plugin';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import * as importPlugin from 'eslint-plugin-import';
-import jest from 'eslint-plugin-jest';
-import promise from 'eslint-plugin-promise';
-import n from 'eslint-plugin-n';
-import next from '@next/eslint-plugin-next';
-import globals from 'globals';
-import prettier from 'eslint-plugin-prettier';
-
-import eslintPrettier from 'eslint-config-prettier';
-import * as typescriptParser from '@typescript-eslint/parser';
-
-import eslintRules from './rules/eslint.mjs';
-import reactRules from './rules/react.mjs';
-import reactHooksRules from './rules/reactHooks.mjs';
-import typescriptRules from './rules/typescript.mjs';
-import jsxA11yRules from './rules/jsx-a11y.mjs';
-import importRules from './rules/import.mjs';
-import jestRules from './rules/jest.mjs';
-import promiseRules from './rules/promise.mjs';
-import nRules from './rules/n.mjs';
-import nextRules from './rules/next.mjs';
-import prettierRules from './rules/prettier.mjs';
-import eslintTypescriptRules from './rules/eslint-typescript.mjs';
+import harmony from './src/index.mjs';
 
 const config = [
-  importPlugin.configs.typescript,
+  ...harmony,
   {
     languageOptions: {
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parser: typescriptParser,
       parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
         project: './tsconfig.json',
       },
-    },
-    files: [
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.json',
-      '**/*.mjs',
-      '**/*.cjs',
-    ],
-    plugins: {
-      prettier,
-      react,
-      'react-hooks': reactHooks,
-      'jsx-a11y': jsxA11y,
-      import: importPlugin,
-      promise,
-      n,
-      '@next/next': next,
-    },
-    rules: {
-      ...eslintRules,
-      ...reactRules,
-      ...reactHooksRules,
-      ...jsxA11yRules,
-      ...importRules,
-      ...promiseRules,
-      ...nRules,
-      ...nextRules,
-      ...prettierRules,
-      ...eslintPrettier.rules,
-    },
-  },
-  {
-    files: ['**/*.d.ts'],
-    rules: {
-      'import/unambiguous': 'off',
-    },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      '@typescript-eslint': typescript,
-    },
-    rules: {
-      ...eslintTypescriptRules,
-      ...typescriptRules,
-    },
-  },
-  {
-    files: ['**/*.test.js', '**/*.test.jsx', 'tests/**/*.js', 'tests/**/*.jsx'],
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-      },
-    },
-    plugins: {
-      jest,
-    },
-    rules: {
-      ...jestRules,
     },
   },
 ];
