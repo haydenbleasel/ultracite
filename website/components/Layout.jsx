69b6244f4321e7ebe082397afe31e288424c8f08
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-21 23:39:16 -0400

    Remove unused components

--- website/components/Layout.jsx
+++ website/components/Layout.jsx
@@ -1,14 +1,7 @@
-'use client';
-
-import { useId } from 'react'
-
 import { Intro, IntroFooter } from './Intro'
-import { StarField } from './StarField'
 import { ThemeToggle } from './ThemeToggle'
 
 function Timeline() {
-  let id = useId()
-
   return (
     <div className="pointer-events-none absolute inset-0 z-50 overflow-hidden lg:right-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] lg:overflow-visible">
       <svg
@@ -16,7 +9,12 @@ function Timeline() {
         aria-hidden="true"
       >
         <defs>
-          <pattern id={id} width="6" height="8" patternUnits="userSpaceOnUse">
+          <pattern
+            id="timeline"
+            width="6"
+            height="8"
+            patternUnits="userSpaceOnUse"
+          >
             <path
               d="M0 0H6M0 8H6"
               className="stroke-sky-900/10 dark:stroke-white/10 xl:stroke-white/10"
@@ -24,62 +22,20 @@ function Timeline() {
             />
           </pattern>
         </defs>
-        <rect width="100%" height="100%" fill={`url(#${id})`} />
+        <rect width="100%" height="100%" fill={`url(#timeline)`} />
       </svg>
     </div>
   )
 }
 
-function Glow() {
-  let id = useId()
-
-  return (
-    <div className="absolute inset-0 -z-10 overflow-hidden bg-gray-950 lg:right-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem]">
-      <svg
-        className="absolute -bottom-48 left-[-40%] h-[80rem] w-[180%] lg:-right-40 lg:bottom-auto lg:left-auto lg:top-[-40%] lg:h-[180%] lg:w-[80rem]"
-        aria-hidden="true"
-      >
-        <defs>
-          <radialGradient id={`${id}-desktop`} cx="100%">
-            <stop offset="0%" stopColor="rgba(56, 189, 248, 0.3)" />
-            <stop offset="53.95%" stopColor="rgba(0, 71, 255, 0.09)" />
-            <stop offset="100%" stopColor="rgba(10, 14, 23, 0)" />
-          </radialGradient>
-          <radialGradient id={`${id}-mobile`} cy="100%">
-            <stop offset="0%" stopColor="rgba(56, 189, 248, 0.3)" />
-            <stop offset="53.95%" stopColor="rgba(0, 71, 255, 0.09)" />
-            <stop offset="100%" stopColor="rgba(10, 14, 23, 0)" />
-          </radialGradient>
-        </defs>
-        <rect
-          width="100%"
-          height="100%"
-          fill={`url(#${id}-desktop)`}
-          className="hidden lg:block"
-        />
-        <rect
-          width="100%"
-          height="100%"
-          fill={`url(#${id}-mobile)`}
-          className="lg:hidden"
-        />
-      </svg>
-      <div className="absolute inset-x-0 bottom-0 right-0 h-px bg-white mix-blend-overlay lg:left-auto lg:top-0 lg:h-auto lg:w-px" />
-    </div>
-  )
-}
 
 function FixedSidebar({ main, footer }) {
   return (
     <div className="relative flex-none overflow-hidden px-6 lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex lg:px-0">
-      <Glow />
-      <div className="relative flex w-full lg:pointer-events-auto lg:mr-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] lg:overflow-y-auto lg:pl-[max(4rem,calc(50%-38rem))]">
+      <div className="relative flex w-full bg-neutral-950 lg:pointer-events-auto lg:mr-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] lg:overflow-y-auto lg:pl-[max(4rem,calc(50%-38rem))]">
         <div className="mx-auto max-w-lg lg:mx-0 lg:flex lg:w-96 lg:max-w-none lg:flex-col lg:before:flex-1 lg:before:pt-6">
           <div className="pb-16 pt-20 sm:pb-20 sm:pt-32 lg:py-20">
-            <div className="relative">
-              <StarField />
-              {main}
-            </div>
+            <div className="relative">{main}</div>
           </div>
           <div className="flex flex-1 items-end justify-center pb-4 lg:justify-start lg:pb-6">
             {footer}
