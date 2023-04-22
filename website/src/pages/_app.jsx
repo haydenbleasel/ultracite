371d1b23b3a84a9c01b4f7d8f2d5f9841ef9a89c
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-21 22:56:11 -0400

    Add tailwind-commit template

---
+++ website/src/pages/_app.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import { ThemeProvider } from 'next-themes'
+import { MDXProvider } from '@mdx-js/react'
+
+import { Layout } from '@/components/Layout'
+import * as mdxComponents from '@/components/mdx'
+
+import '@/styles/tailwind.css'
+import 'focus-visible'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Commit - Open-source Git client for macOS minimalists</title>
+        <meta
+          name="description"
+          content="Commit is a lightweight Git client you can open from anywhere any time you’re ready to commit your work with a single keyboard shortcut. It’s fast, beautiful, and completely unnecessary."
+        />
+      </Head>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <MDXProvider components={mdxComponents}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MDXProvider>
+      </ThemeProvider>
+    </>
+  )
+}
