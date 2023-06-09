cad9559c8c90dd118e4c470202544d5beea4d64f
Hayden Bleasel <hello@haydenbleasel.com>
2023-06-08 22:44:12 -0400

    Switch jsx-a11y to opt-in method

--- rules/jsx-a11y.mjs
+++ rules/jsx-a11y.mjs
@@ -1,37 +1,21 @@
 /* eslint-disable import/no-anonymous-default-export */
 
-export default {
-  // JSX a11y
-  'jsx-a11y/alt-text': 'error',
-  'jsx-a11y/anchor-has-content': 'error',
-  'jsx-a11y/anchor-is-valid': 'error',
-  'jsx-a11y/aria-activedescendant-has-tabindex': 'error',
-  'jsx-a11y/aria-props': 'error',
-  'jsx-a11y/aria-proptypes': 'error',
-  'jsx-a11y/aria-role': 'error',
-  'jsx-a11y/aria-unsupported-elements': 'error',
-  'jsx-a11y/autocomplete-valid': 'error',
-  'jsx-a11y/click-events-have-key-events': 'error',
-  'jsx-a11y/heading-has-content': 'error',
-  'jsx-a11y/html-has-lang': 'error',
-  'jsx-a11y/iframe-has-title': 'error',
-  'jsx-a11y/img-redundant-alt': 'error',
-  'jsx-a11y/interactive-supports-focus': 'error',
-  'jsx-a11y/label-has-associated-control': 'error',
-  'jsx-a11y/media-has-caption': 'error',
-  'jsx-a11y/mouse-events-have-key-events': 'error',
-  'jsx-a11y/no-access-key': 'error',
+import plugin from 'eslint-plugin-jsx-a11y';
+
+const { rules } = plugin;
+
+const availableKeys = Object.keys(rules).filter(
+  (key) => !rules[key].meta.deprecated
+);
+
+const baseRules = Object.fromEntries(
+  availableKeys.map((key) => [`jsx-a11y/${key}`, 'error'])
+);
+
+const overrideRules = {
   'jsx-a11y/no-autofocus': 'off',
-  'jsx-a11y/no-distracting-elements': 'error',
-  'jsx-a11y/no-interactive-element-to-noninteractive-role': 'error',
-  'jsx-a11y/no-noninteractive-element-interactions': 'error',
-  'jsx-a11y/no-noninteractive-element-to-interactive-role': 'error',
-  'jsx-a11y/no-noninteractive-tabindex': 'error',
-  'jsx-a11y/no-onchange': 'error',
-  'jsx-a11y/no-redundant-roles': 'error',
-  'jsx-a11y/no-static-element-interactions': 'error',
-  'jsx-a11y/role-has-required-aria-props': 'error',
-  'jsx-a11y/role-supports-aria-props': 'error',
-  'jsx-a11y/scope': 'error',
-  'jsx-a11y/tabindex-no-positive': 'error',
 };
+
+const config = Object.assign(baseRules, overrideRules);
+
+export default config;
