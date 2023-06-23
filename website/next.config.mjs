5f21abe6217c48ed83fbfca5bd3dd49eb6024a42
Hayden Bleasel <hello@haydenbleasel.com>
2023-06-23 00:03:46 -0400

    Update next.config.mjs

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,9 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    appDir: true,
     scrollRestoration: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
