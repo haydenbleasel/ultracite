c61e8086738f6a3b4d2c81787189053b99b7d87a
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-27 15:58:30 -0400

    Fix baseUrl on website

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -4,13 +4,22 @@ import { Changelog } from './components/changelog';
 import type { Metadata } from 'next';
 import type { FC } from 'react';
 
+const projectUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL;
+const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+
+if (!projectUrl) {
+  throw new Error('Missing VERCEL_PROJECT_PRODUCTION_URL');
+}
+
 export const generateMetadata = async (): Promise<Metadata> => {
   const repo = await getRepo();
 
   return {
     title: 'Ultracite | Hayden Bleasel',
     description: repo.data.description,
-    metadataBase: new URL(process.env.VERCEL_PROJECT_PRODUCTION_URL ?? ''),
+    metadataBase: new URL(
+      `${protocol}://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    ),
   };
 };
 
