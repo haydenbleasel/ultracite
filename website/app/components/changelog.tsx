70ce50f081912b225e090ddf2fe0862f727653d4
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-27 19:32:30 -0400

    Bundle release notes on website on the same day

--- website/app/components/changelog.tsx
+++ website/app/components/changelog.tsx
@@ -14,32 +14,60 @@ import type { ReactElement } from 'react';
 export const Changelog = async (): Promise<ReactElement> => {
   const changelog = await getChangelog();
 
+  const releasesByDay = changelog.data.reduce<
+    Record<string, typeof changelog.data>
+  >((acc, release) => {
+    const date = new Date(release.created_at).toDateString();
+    if (!(date in acc)) {
+      acc[date] = [];
+    }
+    acc[date].push(release);
+    return acc;
+  }, {});
+
   return (
     <div className="relative md:overflow-y-auto md:h-screen py-20 px-4">
       <Accordion
         type="single"
         className="prose prose-sm prose-neutral max-w-lg mx-auto"
-        defaultValue={String(changelog.data[0].id)}
+        defaultValue={Object.keys(releasesByDay)[0]}
         collapsible
       >
-        {changelog.data.map((release) => (
-          <AccordionItem
-            value={String(release.id)}
-            key={release.id}
-            className="[&>h3]:m-0"
-          >
+        {Object.entries(releasesByDay).map(([date, releases]) => (
+          <AccordionItem value={date} key={date} className="[&>h3]:m-0">
             <AccordionTrigger>
-              <p className="m-0 flex-1 text-left">{release.name}</p>
-              <time dateTime={release.created_at} className="shrink-0 m-0 mr-2">
-                {dateFormatter.format(new Date(release.created_at))}
+              <div className="flex items-center gap-1 flex-1 text-left">
+                <p className="m-0">{releases[0].name}</p>
+                {releases.length > 1 && (
+                  <span className="font-normal text-neutral-500 dark:text-neutral-400">
+                    and {releases.length - 1} other release
+                    {releases.length > 2 ? 's' : ''}
+                  </span>
+                )}
+              </div>
+              <time
+                dateTime={releases[0].created_at}
+                className="shrink-0 m-0 mr-2"
+              >
+                {dateFormatter.format(new Date(releases[0].created_at))}
               </time>
             </AccordionTrigger>
             <AccordionContent>
               <div
                 dangerouslySetInnerHTML={{
-                  __html: release.body ? marked.parse(release.body) : '',
+                  __html: releases[0].body
+                    ? marked.parse(releases[0].body)
+                    : '',
                 }}
               />
+              <h4>Other releases</h4>
+              <ul>
+                {releases.slice(1).map((release) => (
+                  <li key={release.id}>
+                    <a href={release.html_url}>{release.name}</a>
+                  </li>
+                ))}
+              </ul>
             </AccordionContent>
           </AccordionItem>
         ))}
