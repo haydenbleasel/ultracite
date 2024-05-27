c9940a0c8c2469b50366570b016776db15e6165e
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-27 14:54:55 -0400

    Install shadcn/ui accordion

--- website/components/button.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC, ComponentProps } from 'react';
-
-type ButtonProps = ComponentProps<'button'>;
-
-export const Button: FC<ButtonProps> = (props) => (
-  <button type="button" {...props} />
-);
--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,4 +1,5 @@
 import typography from '@tailwindcss/typography';
+import animate from 'tailwindcss-animate';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -8,7 +9,28 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
       colors: {
         orange: {
           50: '#fff8f3',
@@ -27,11 +49,6 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-geist-sans)'],
         mono: ['var(--font-geist-mono)'],
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       typography: (theme: (path: string) => string) => ({
         DEFAULT: {
@@ -67,7 +84,7 @@ const config: Config = {
       }),
     },
   },
-  plugins: [typography],
+  plugins: [typography, animate],
 };
 
 export default config;
