e312e3a9363e0fe08b0800bdebaeae19e50bb4f8
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-27 16:22:29 -0400

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['eslint.config.mjs'],
+  entry: ['eslint.config.mjs', 'prettier.js', 'stylelint.js'],
   sourcemap: false,
   minify: true,
   dts: true,
   format: ['esm'],
+  splitting: false,
 });
