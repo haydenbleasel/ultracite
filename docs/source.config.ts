7746be641720e2fff638e033dddc6614c6570097
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-02 18:54:56 -0700

    Install and implement package-install

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -1,12 +1,12 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { remarkInstall } from 'fumadocs-docgen';
+import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
 
-// Options: https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
   dir: 'content/docs',
 });
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+    remarkPlugins: [remarkInstall],
   },
 });
