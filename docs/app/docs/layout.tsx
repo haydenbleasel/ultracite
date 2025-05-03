c83a67b8076de0c77512882f42153573d1488db3
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-02 18:05:49 -0700

    Switch to Notebook layout

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -1,11 +1,18 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout
+      {...baseOptions}
+      // the position of navbar
+      nav={{ ...baseOptions.nav, mode: 'top' }}
+      // the position of Sidebar Tabs
+      tabMode="navbar"
+      tree={source.pageTree}
+    >
       {children}
     </DocsLayout>
   );
