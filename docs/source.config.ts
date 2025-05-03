ffd9dda8ed1947e9c11a81c32cecac32f5d542e5
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-02 18:55:20 -0700

    Flatten docs folders

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -2,7 +2,7 @@ import { remarkInstall } from 'fumadocs-docgen';
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
 
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: 'content',
 });
 
 export default defineConfig({
