cfb0716b22f9ad76dbea430bd04f5e597c14fd23
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-02 18:12:23 -0700

    Fix fonts, migrate logo

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -2,17 +2,29 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
-
-const inter = Inter({
-  subsets: ['latin'],
-});
+import { Geist, Geist_Mono } from 'next/font/google';
+import { cn } from 'fumadocs-ui/utils/cn';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
+const sans = Geist({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+const mono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
+
 const Layout = ({ children }: LayoutProps) => (
-  <html lang="en" className={inter.className} suppressHydrationWarning>
+  <html lang="en" className={cn(
+    'touch-manipulation font-sans antialiased',
+    sans.variable,
+    mono.variable
+  )} suppressHydrationWarning>
     <body className="flex flex-col min-h-screen">
       <RootProvider>{children}</RootProvider>
     </body>
