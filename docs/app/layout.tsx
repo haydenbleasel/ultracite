5528e91e05f2170b19e7ff2afa98ebd94bc4f387
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-02 18:07:58 -0700

    Misc fixes

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -7,12 +7,16 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
-  );
-}
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => (
+  <html lang="en" className={inter.className} suppressHydrationWarning>
+    <body className="flex flex-col min-h-screen">
+      <RootProvider>{children}</RootProvider>
+    </body>
+  </html>
+);
+
+export default Layout;
