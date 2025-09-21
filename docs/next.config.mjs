e945e98d15edb5aa980a5b8a9383fa44ad06f3c6
Hayden Bleasel <hello@haydenbleasel.com>
2025-09-20 17:32:37 -0700

    Finish wiring up page actions

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -13,7 +13,7 @@ const config = {
   async rewrites() {
     return [
       {
-        source: '/docs/:path*.mdx',
+        source: '/:path*.mdx',
         destination: '/llms.mdx/:path*',
       },
     ];
