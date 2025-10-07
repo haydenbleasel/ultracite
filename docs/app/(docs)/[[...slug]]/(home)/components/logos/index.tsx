5f445a7a43b72f79e0a55e013da54bb80cd06933
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-06 23:08:46 -0700

    Update logos

--- docs/app/(docs)/[[...slug]]/(home)/components/logos/index.tsx
+++ docs/app/(docs)/[[...slug]]/(home)/components/logos/index.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Adobe from "./adobe.svg";
+import Arcade from "./arcade.svg";
 import AlanTuringInstitute from "./ati.svg";
+import Axiom from "./axiom.svg";
 import Consent from "./consent.svg";
+import FrenchGovernment from "./french-government.svg";
 import Profound from "./profound.svg";
+import Tencent from "./tencent.svg";
 import VA from "./va.svg";
 import Vercel from "./vercel.svg";
 
@@ -31,6 +35,22 @@ const logos = [
     name: "VA",
     src: VA,
   },
+  {
+    name: "French Government",
+    src: FrenchGovernment,
+  },
+  {
+    name: "Tencent",
+    src: Tencent,
+  },
+  {
+    name: "Arcade",
+    src: Arcade,
+  },
+  {
+    name: "Axiom",
+    src: Axiom,
+  },
 ];
 
 export const Logos = () => (
@@ -47,7 +67,7 @@ export const Logos = () => (
       </a>{" "}
       of open source projects.
     </p>
-    <div className="mx-auto grid max-w-5xl grid-cols-2 flex-col items-center gap-16 sm:grid-cols-3 md:grid-cols-6">
+    <div className="mx-auto grid max-w-5xl grid-cols-2 flex-col items-center gap-16 sm:grid-cols-3 md:grid-cols-5">
       {logos.map((item) => (
         <Image
           alt=""
