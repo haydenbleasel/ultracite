ad7a9453f8f96ca3bdd2daa3b7bd7760a6f6efb9
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-02 19:45:31 -0800

    Update IDE graphic

--- apps/docs/app/(docs)/[[...slug]]/(home)/components/ide/sidebar.tsx
+++ apps/docs/app/(docs)/[[...slug]]/(home)/components/ide/sidebar.tsx
@@ -1,6 +1,7 @@
 import {
   SiCss,
   SiJson,
+  SiMarkdown,
   SiReact,
   SiTypescript,
 } from "@icons-pack/react-simple-icons";
@@ -52,6 +53,10 @@ export const Sidebar = () => (
     <div className="flex flex-col gap-px">
       <FileItem icon={FolderIcon} name=".cursor" />
       <FileItem className="ml-2" icon={SiJson} name="hooks.json" />
+      <FileItem className="ml-2" icon={FolderIcon} name=".rules" />
+      <FileItem className="ml-4" icon={SiMarkdown} name="ultracite.mdc" />
+      <FileItem icon={FolderIcon} name=".vscode" />
+      <FileItem className="ml-2" icon={SiJson} name="settings.json" />
     </div>
     {files.map((file) => (
       <FileItem icon={file.icon} key={file.name} name={file.name} />
