75eab58f26a9bf68b2d1d29d03d0d4d3ce391fc1
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-26 11:23:21 -0800

    Bump ESLint deps, re-enable Tailwind rules

--- packages/cli/config/eslint/core/eslint.config.mjs
+++ packages/cli/config/eslint/core/eslint.config.mjs
@@ -4,7 +4,6 @@ import typescript from "@typescript-eslint/eslint-plugin";
 // biome-ignore lint/performance/noNamespaceImport: Required for ESLint parser compatibility
 import * as typescriptParser from "@typescript-eslint/parser";
 import eslintPrettier from "eslint-config-prettier";
-// import tailwindcss from 'eslint-plugin-tailwindcss';
 // biome-ignore lint/performance/noNamespaceImport: Required for ESLint plugin compatibility
 import * as importTypescriptResolver from "eslint-import-resolver-typescript";
 import compat from "eslint-plugin-compat";
@@ -19,6 +18,7 @@ import prettier from "eslint-plugin-prettier";
 import promise from "eslint-plugin-promise";
 import sonarjs from "eslint-plugin-sonarjs";
 import storybook from "eslint-plugin-storybook";
+import tailwindcss from "eslint-plugin-tailwindcss";
 import unicorn from "eslint-plugin-unicorn";
 import unusedImports from "eslint-plugin-unused-imports";
 import globals from "globals";
@@ -34,9 +34,9 @@ import prettierRules from "./rules/prettier.mjs";
 import promiseRules from "./rules/promise.mjs";
 import sonarjsRules from "./rules/sonarjs.mjs";
 import storybookRules from "./rules/storybook.mjs";
+import tailwindcssRules from "./rules/tailwindcss.mjs";
 import typescriptRules from "./rules/typescript.mjs";
 import unicornRules from "./rules/unicorn.mjs";
-// import tailwindcssRules from './rules/tailwindcss.mjs';
 import unusedImportsRules from "./rules/unused-imports.mjs";
 
 const config = [
@@ -70,7 +70,7 @@ const config = [
       promise,
       n,
       "unused-imports": unusedImports,
-      // tailwindcss,
+      tailwindcss,
       sonarjs,
       compat,
       unicorn,
@@ -84,7 +84,7 @@ const config = [
       ...prettierRules,
       ...eslintPrettier.rules,
       ...unusedImportsRules,
-      // ...tailwindcssRules,
+      ...tailwindcssRules,
       ...sonarjsRules,
       ...compatRules,
       ...unicornRules,
