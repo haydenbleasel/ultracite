71839d0eac786f754253b72c894834e7c262eeda
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-26 10:49:08 -0800

    Add support for Astro, Angular, Solid, Svelte and Vue

---
+++ packages/cli/config/eslint/astro/eslint.config.mjs
@@ -0,0 +1,18 @@
+/* eslint-disable n/no-unpublished-import, n/no-extraneous-import, import/no-extraneous-dependencies, id-length */
+
+import astro from "eslint-plugin-astro";
+import astroRules from "./rules/astro.mjs";
+
+const config = [
+  {
+    files: ["**/*.astro"],
+    plugins: {
+      astro,
+    },
+    rules: {
+      ...astroRules,
+    },
+  },
+];
+
+export default config;
