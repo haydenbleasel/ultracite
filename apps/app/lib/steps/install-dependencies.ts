ac09b2e1925310031482f8ef54d8e236b56ad9a8
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-31 13:10:40 -0800

    Update install-dependencies.ts

--- apps/app/lib/steps/install-dependencies.ts
+++ apps/app/lib/steps/install-dependencies.ts
@@ -4,6 +4,13 @@ export async function installDependencies(sandboxId: string): Promise<void> {
   "use step";
 
   const sandbox = await Sandbox.get({ sandboxId });
+
+  // We need to install all the relevant package managers
+  await sandbox.runCommand("npm", ["install", "-g", "pnpm"]);
+  await sandbox.runCommand("npm", ["install", "-g", "yarn"]);
+  await sandbox.runCommand("npm", ["install", "-g", "bun"]);
+
+  // We use `ni` to install dependencies by automatically detecting the package manager.
   await sandbox.runCommand("npm", ["install", "-g", "@antfu/ni"]);
   await sandbox.runCommand("ni", []);
 }
