a8408b65e5f73e34804cebcb9a6eb5cd20e114ec
Hayden Bleasel <hello@haydenbleasel.com>
2026-01-01 03:04:20 -0800

    Resolves #482

---
+++ .changeset/plain-walls-buy.md
@@ -0,0 +1,5 @@
+---
+"ultracite": patch
+---
+
+Fix bundling issues
--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -10,4 +10,5 @@ export default defineConfig({
   banner: {
     js: "#!/usr/bin/env node",
   },
+  noExternal: ["@repo/data"],
 });
