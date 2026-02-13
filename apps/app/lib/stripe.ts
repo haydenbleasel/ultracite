423a58afdb6ca5607003425dab7eec0e5a099170
Hayden Bleasel <hello@haydenbleasel.com>
2026-02-13 10:44:14 -0800

    Update stripe.ts

--- apps/app/lib/stripe.ts
+++ apps/app/lib/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 import { env } from "./env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-12-15.clover",
+  apiVersion: "2026-01-28.clover",
 });
