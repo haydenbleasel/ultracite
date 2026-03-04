5f16b295a6ffb2cdd2a0a7e8530b9ba2910d1e81
Hayden Bleasel <hello@haydenbleasel.com>
2026-03-04 14:38:20 -0800

    Bump deps

--- apps/app/lib/stripe.ts
+++ apps/app/lib/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 import { env } from "./env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2026-01-28.clover",
+  apiVersion: "2026-02-25.clover",
 });
