4b0a4cf7da6979f96148ec320286d2df5d36c966
Hayden Bleasel <hello@haydenbleasel.com>
2026-03-05 12:08:25 -0800

    Delete app

--- apps/app/lib/referral/constants.ts
+++
@@ -1,2 +0,0 @@
-export const REFERRAL_CREDIT_CENTS = 500; // $5.00
-export const REFERRAL_COOKIE = "ultracite_referral";
--- apps/app/lib/stripe.ts
+++
@@ -1,6 +0,0 @@
-import Stripe from "stripe";
-import { env } from "./env";
-
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2026-02-25.clover",
-});
